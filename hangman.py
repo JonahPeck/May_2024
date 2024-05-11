c1abf9a077ea7c8edc7c77b8dc90f6570141a4ed
JonahPeck <PeckJonahC@gmail.com>
2024-05-10 22:16:19 -0400

    letter chosen positioning

---
+++ hangman.py
@@ -0,0 +1,30 @@
+import random
+
+
+
+word_list = ["ardvark", "camel", "bacon"]
+chosen_word = random.choice(word_list)
+
+display = []
+word_length = len(chosen_word)
+
+print("chosen word is " + chosen_word)
+
+for i in range(word_length):
+    display += "_"
+print(display)
+
+guess = input("Guess a letter: ").lower()
+
+
+print(" ")
+
+for position in range(0, word_length):
+    letter = chosen_word[position]
+    if letter == guess:
+        display[position] = letter
+print(display)
+        #want this to then display the guessed character in sequqnce of word chosen 
+
+
+        
