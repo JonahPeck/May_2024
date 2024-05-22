4ba12d9652a8bc1c37d5f05a29fc658daac36f86
JonahPeck <PeckJonahC@gmail.com>
2024-05-22 17:53:55 -0400

    ending

--- hangman.py
+++ hangman.py
@@ -59,7 +59,7 @@ stages = ['''
 end_of_game = False
 word_list = ["ardvark", "camel", "bacon"]
 chosen_word = random.choice(word_list)
-lives = 
+lives = 6
 display = []
 word_length = len(chosen_word)
 
@@ -80,11 +80,23 @@ while not end_of_game:
         letter = chosen_word[position]
         if letter == guess:
             display[position] = letter
+        else:
+            lives -= 1
+            print(stages[lives])
+            if lives == 0:
+                break
     print(display)
 
     if "_" not in display:
         end_of_game = True
         print("You win")
+    
+    if letter != guess:
+        print(stages[lives])
+
+print("You lose")
+
+    
 
 
 #cndijcndijcni
